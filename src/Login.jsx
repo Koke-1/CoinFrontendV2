4e918a03e621b61afb718d34c477d4a8d2a1c19f
Koke <sanjuangeorge@gmail.com>
2023-11-13 10:43:31 -0600

    gh pages

--- src/Login.jsx
+++ src/Login.jsx
@@ -33,7 +33,7 @@ export default function Login() {
         pass.value = "" 
         seterror(false)
       try {
-        const {data} =  await axios.get(`https://coinbackend.onrender.com/API`,{params:{userName:username,password:password}}).then((result)=> console.log(result)).catch((error)=> console.log(error))
+        const {data} =  await axios.get(`https://coinbackend.onrender.com/API`,{params:{userName:username,password:password}}).then((result)=> console.log(result.data)).catch((error)=> console.log(error))
         console.log(data.data);
         
         if (data.data.length <= 0) {
